f2a42cd21819da7ef47303e609cdbbb6fd4e2b70
Frederick <frederick.gfl@gmail.com>
2020-01-07 23:45:24 +0000

    Add 2 queries for average duration
    - average duration for each cohort
    - longest average duration for cohorts

---
+++ 4_queries/5_longest_average_cohort_assist_duration.sql
@@ -0,0 +1,8 @@
+SELECT cohorts.name AS name
+     , AVG(completed_at - started_at) AS average_assistance_time
+  FROM assistance_requests AS ar
+    JOIN students AS s ON ar.student_id = s.id
+    JOIN cohorts ON s.cohort_id = cohorts.id
+  GROUP BY cohorts.name
+  ORDER BY average_assistance_time DESC
+  LIMIT 1;
